86b03e13f7932d057134d16dbb50645439122596
ABVenu <avenugopalb@gmail.com>
2024-12-05 16:36:23 +0530

    feat/login

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-let userNames = ["Alice","Bob", "Charlie", "Delta", "Stephen"];
-function signup(str){
-    let filetredData = userNames.filter((el,i)=> el==str);
-   if(filetredData.length==0){
+let userNames = ["Alice", "Bob", "Charlie", "Delta", "Stephen"];
+function signup(str) {
+  let filetredData = userNames.filter((el, i) => el == str);
+  if (filetredData.length == 0) {
     /// i need to push
-    userNames.push(str)
-    console.log("Signup Sucessfull.....")
-   }else{
-    console.log("Already Registred....")
-   }
+    userNames.push(str);
+    console.log("Signup Sucessfull.....");
+  } else {
+    console.log("Already Registred....");
+  }
 }
-signup("Venu")
-signup("Venu")
+signup("Venu");
+signup("Venu");
+
+function login(str, password) {
+  let filetredData = userNames.filter((el, i) => el == str);
+  if (filetredData.length == 0) {
+    /// i need to push
+    console.log("User Not Found...Please Signup");
+  } else {
+    if (password == "Emp@123") {
+      console.log("Login Sucessfull.....");
+    } else {
+      console.log("Wrong Password....");
+    }
+  }
+}
+login("Zebra", "emp@123");
